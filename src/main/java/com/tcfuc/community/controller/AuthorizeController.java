90d2798baf4a61f8bed3b2a940d008aeae5e503b
tcfuc <1141614570@qq.com>
2020-01-12 19:56:00 +0800

    extract github config from code to properties

--- src/main/java/com/tcfuc/community/controller/AuthorizeController.java
+++ src/main/java/com/tcfuc/community/controller/AuthorizeController.java
@@ -3,11 +3,14 @@ package com.tcfuc.community.controller;
 import com.tcfuc.community.dto.AccessTokenDTO;
 import com.tcfuc.community.dto.GitHubUser;
 import com.tcfuc.community.provider.GitHubProvider;
+import org.apache.catalina.filters.ExpiresFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class AuthorizeController {
@@ -24,7 +27,8 @@ public class AuthorizeController {
 
     @RequestMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -33,8 +37,14 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
         GitHubUser user = gitHubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        if(user != null){
+            //登录成功，写cookie 和 session
+            request.getSession().setAttribute("user", user);
+            return "redirect:/";
+        } else {
+            //登录失败，重新登录
+            return "redirect:/";
+        }
     }
 
 
