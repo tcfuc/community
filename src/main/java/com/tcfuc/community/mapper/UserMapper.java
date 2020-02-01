db3d58445d178f709573ab48002e10e703e4d52c
tcfuc <1141614570@qq.com>
2020-02-01 19:53:05 +0800

    add sql ddl

---
+++ src/main/java/com/tcfuc/community/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package com.tcfuc.community.mapper;
+
+/**
+ * @author zhou
+ * @date 2020/1/22
+ * @time 21:11
+ */
+public class UserMapper {
+}
---
+++ src/main/java/com/tcfuc/community/model/User.java
@@ -0,0 +1,9 @@
+package com.tcfuc.community.model;
+
+/**
+ * @author zhou
+ * @date 2020/1/22
+ * @time 21:11
+ */
+public class User {
+}
