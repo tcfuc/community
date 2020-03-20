aca7e4af25f23b1932f614772fbf5433601ed8a8
tcfuc <1141614570@qq.com>
2020-03-20 19:05:57 +0800

    修复脚本

--- src/main/resources/db/migration/V3__Create_comment_table.sql
+++ src/main/resources/db/migration/V3__Create_comment_table.sql
@@ -3,12 +3,10 @@ create table comment
 	id bigint auto_increment,
 	parent_id bigint,
 	type int,
-	commentator commentator varchar(100),
+	commentator varchar(100),
 	content varchar(1024),
 	comment_count int default 0,
 	gmt_modified bigint,
 	gmt_create bigint,
-	like_count bigint default 0,
-	constraint comment_pk
-		primary key (id)
+	like_count bigint default 0
 );
--- src/main/resources/db/migration/V5__Create_notification_table.sql
+++ src/main/resources/db/migration/V5__Create_notification_table.sql
@@ -1,12 +1,10 @@
 create table notification
 (
-	id bigint auto_increment,
+	id bigint auto_increment primary key,
 	notifier varchar (100),
 	receiver varchar (100),
 	outerid bigint,
 	type int,
 	gmt_create bigint,
-	status int default 0,
-	constraint notification_pk
-		primary key (id)
+	status int default 0
 );
